45c5d97ad529309713b3e5e0797d59732f753aae
Lucas Santos <lucasljsb@gmail.com>
2018-12-06 16:53:47 -0300

    Construída biblioteca de integração com o Doctrine

---
+++ dist/application/libraries/Doctrine.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Biblioteca que permite integração com o Doctrine.
+ * @author Daniel Ferreira
+ * @author Guilherme do Valle
+ * @author Lucas Santos
+ * @since	Version 1.0.0
+ */
+
+use Doctrine\Common\ClassLoader;
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+class Doctrine {
+
+    /**
+     * Gerenciador de entidades
+     */
+    private $entity_manager;
+
+    public function __construct()
+    {
+        // Carrega e aplica as configurações de banco de dados
+        require APPPATH . 'config/database.php';
+        $connection_options = array(
+            'driver'        => 'pdo_mysql',
+            'user'          => $db['default']['username'],
+            'password'      => $db['default']['password'],
+            'host'          => $db['default']['hostname'],
+            'dbname'        => $db['default']['database'],
+            'charset'       => $db['default']['char_set'],
+            'driverOptions' => array(
+                'charset'   => $db['default']['char_set'],
+            ),
+        );
+
+        // As linhas a seguir definem o namespace padrão para as entidades e os diretórios onde esses se encontram
+        $models_namespace = 'Entity';
+        $models_path = APPPATH . 'models';
+        $metadata_paths = array(APPPATH . 'models');
+
+        // Aplica as configurações com base nas informações definidas acima
+        // Se $dev_mode for definido como TRUE o cache é desativado
+        $config = Setup::createAnnotationMetadataConfiguration($metadata_paths, $dev_mode = true, $proxies_dir);
+        $this->entity_manager = EntityManager::create($connection_options, $config);
+        $loader = new ClassLoader($models_namespace, $models_path);
+        $loader->register();
+    }
+
+    /**
+     * Get gerenciador de entidades
+     */ 
+    public function get_entity_manager()
+    {
+        return $this->entity_manager;
+    }
+
+}
