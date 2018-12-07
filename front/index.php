70571c060379d7b2a5bc0bb90dcc210c714c4d1a
Lucas Santos <lucasljsb@gmail.com>
2018-12-07 18:29:46 -0300

    Montado o template básico do cabeçalho

--- front/index.php
+++ front/index.php
@@ -1,17 +1,51 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title></title>
+		<title>sisbolão - Gerenciador de apostas</title>
 		<meta name="description" content="">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="">
+		<link rel="stylesheet" href="../dist/public/css/utils.min.css">
+		<link rel="stylesheet" href="../dist/public/css/style.min.css">
 	</head>
 	<body>
-		
+		<header id="top-header" class="bg-white text-uppercase">
+			<div class="top-header__container container px-md-2 py-2 rounded">
+				<div class="row m-0">
+					<nav class="navbar navbar-expand-md col-md-4">
+						<a class="navbar-brand" href="#">
+							<img src="../dist/public/img/logo-slogan.svg" class="d-inline-block align-middle" width="200" alt="Logotipo sisbolão">
+						</a>
+					</nav>
+					<nav class="navbar navbar-expand-md col-md-8 navbar-light ">
+						<div class="collapse navbar-collapse">
+							<ul class="navbar-nav mr-auto">
+								<li class="nav-item active px-md-2">
+									<a role="button" href="#!" class="nav-link">Inicio <span class="sr-only">(atual)</span></a>
+								</li>
+								<li class="nav-item px-md-2">
+									<a role="button" href="#!" class="nav-link">Bolões</a>
+								</li>
+								<li class="nav-item px-md-2">
+									<a role="button" href="#!" class="nav-link">Ranking</a>
+								</li>
+								<li class="nav-item px-md-2">
+									<a role="button" href="#!" class="btn btn-md btn-pal-2 text-white">Cadastre-se</a>
+								</li>
+								<li class="nav-item px-md-2">
+								<a role="button" href="#!" class="btn btn-md btn-pal-1 text-white">Login</a>
+								</li>
+							</ul>
+						</div>
+					</nav>
+				</div><!-- /.row !-->
+			</div><!-- /.container !-->
+		</header>
 	</body>
 </html>
