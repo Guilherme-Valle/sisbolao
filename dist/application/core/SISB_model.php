b70ac21e53cd5a395cfaf81beb030ce94c010055
Lucas Santos <lucasljsb@gmail.com>
2018-12-08 20:32:37 -0300

    Criada classe de núcleo

---
+++ dist/application/core/SISB_model.php
@@ -0,0 +1,51 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class SISB_model extends CI_Model {
+
+    const SINGLE_RESULT = 1;
+    const MULTIPLE_RESULTS = 2;
+
+    /**
+     * 
+     * @since   1.0.0
+     */
+    protected $hydration_mode;
+    
+    /**
+     * 
+     * @since   1.0.0
+     */
+    protected $result;
+
+    public function __construct()
+    {
+        $this->hydration_mode = NULL;
+        $this->result = array();
+        if ($this->config->item('rest_mode') === TRUE)
+        {
+            $this->hydration_mode = Doctrine\ORM\Query::HYDRATE_ARRAY;
+        }
+    }
+
+    /**
+     * 
+     * @param   Doctrine\ORM\Query  $query
+     * @return  mixed
+     */
+    final protected function result(Doctrine\ORM\Query $query, $result_mode = self::MULTIPLE_RESULTS)
+    {
+        switch ($result_mode)
+        {
+            case self::MULTIPLE_RESULTS:
+                $this->result = $query->getResult($this->hydration_mode);
+                break;
+            case self::SINGLE_RESULT:
+                $this->result = $query->getSingleResult($this->hydration_mode);
+                break;
+        }
+        return $this->result;
+    }
+
+}
