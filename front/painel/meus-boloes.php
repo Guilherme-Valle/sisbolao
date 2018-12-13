2a2bb60b706a6fb36a74802d2edf7792e932e150
Lucas Santos <lucasljsb@gmail.com>
2018-12-13 15:44:02 -0300

    Criado template básico da página Meus bolões

---
+++ front/painel/meus-boloes.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>sisbolão - Meus Bolões - Painel de Controle</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="../../dist/public/css/utils.min.css">
+		<link rel="stylesheet" href="../../dist/admin/css/style.min.css">
+	</head>
+	<body>
+		<div class="page-wrap page-wrap--opened-sidebar w-100">
+
+			<header id="top-header" class="bg-white">
+				<div class="container-fluid">
+					<div class="top-header__navbar white row justify-content-md-end">
+						<a data-sidebar-toggle="#sidebar-mobile" role="button" class="sidebar-mobile-toggler d-md-none text-center">
+							<i class="fas fa-times fa-lg text-white"></i>
+							<i class="fa fa-bars fa-lg"></i>
+						</a><!-- /.sidebar-mobile-toggler !-->
+						<div class="col-12 d-md-none">
+							<div class="brand-logo text-center">
+								<img src="../../dist/public/img/logo-slogan.svg" alt="Logotipo sisbolão" class="w-full">
+							</div><!-- /.brand-logo !-->
+						</div>
+						<div class="col-md-3 col-lg-4 text-md-right d-none d-md-flex">
+							<ul class="top-header__navbar__menu ml-auto mb-0 list-style-none">
+								<li>
+									<a>
+										<i class="top-header__navbar__menu__i fas fa-user-circle fa-2x text-pal-2-2"></i>
+										<small>administrador</small>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div><!-- /.row !-->
+				</div><!-- /.container !-->
+			</header><!-- /#top-header !-->
+
+			<main id="dashboard-body">
+				<aside id="sidebar" class="sidebar sidebar--opened d-none d-md-block">
+					<header class="sidebar__header row m-0">
+						<div class="brand-logo col-8">
+							<img src="../../dist/public/img/logo-w.svg" alt="Logotipo sisbolão" class="w-100">
+						</div><!-- /.brand-logo !-->
+						<div class="sidebar-toggle col-4 text-center">
+							<a href="#!" role="button" data-sidebar-toggle="#sidebar">
+								<i class="sidebar-toggle__i fas fa-bars text-secondary"></i>
+							</a>
+						</div><!-- /.sidebar-toggle !-->
+					</header><!-- /.sidebar__header !-->
+					<nav class="sidebar__menu py-4 font-weight-light">
+						<ul class="m-0 p-0 list-style-none">
+							<li>
+								<a role="button" class="px-4 py-3" title="Minha conta">
+									<i class="sidebar__menu__i fas fa-cogs fa-lg"></i>
+									<span>Minha conta</span>
+								</a>
+							</li>
+							<li>
+								<div class="px-4">
+									<hr style="border-color: rgba(255,255,255,0.1);">
+								</div>
+							</li>
+							<li>
+								<a role="button" class="px-4 py-3" title="Meus personagens">
+									<i class="sidebar__menu__i far fa-plus-square fa-lg"></i>
+									<span>Criar Bolão</span>
+								</a>
+							</li>
+							<li>
+								<a role="button" class="px-4 py-3" title="Meus personagens">
+									<i class="sidebar__menu__i far fa-futbol fa-lg"></i>
+									<span>Meus Bolões</span>
+								</a>
+							</li>
+							<li>
+								<a role="button" class="px-4 py-3" title="Meus personagens">
+									<i class="sidebar__menu__i fas fa-futbol fa-lg"></i>
+									<span>Bolões que participo</span>
+								</a>
+							</li>
+							<li>
+								<a role="button" class="px-4 py-3" title="Logs">
+									<i class="sidebar__menu__i fas fa-file-alt fa-lg"></i>
+									<span>Histórico</span>
+								</a>
+							</li>
+							<li>
+								<a role="button" class="px-4 py-3" title="Sair">
+									<i class="sidebar__menu__i fas fa-sign-out-alt fa-lg"></i>
+									<span>Sair</span>
+								</a>
+							</li>
+						</ul>
+					</nav>
+				</aside><!-- /#sidebar !-->
+				<aside id="sidebar-mobile" class="sidebar sidebar--closed d-md-none">
+					<header class="sidebar__header py-4 text-center text-light">
+						<i class="fas fa-user-circle fa-2x"></i>
+						<p class="font-weight-light m-0 small">administrador</p>
+					</header><!-- /.sidebar__header !-->
+					<nav class="sidebar__menu py-4 font-weight-light">
+						<ul class="m-0 p-0 list-style-none">
+							<li>
+								<a role="button" class="px-4 py-3" title="Minha conta">
+									<i class="sidebar__menu__i fas fa-cogs fa-lg"></i>
+									<span>Minha conta</span>
+								</a>
+							</li>
+							<li>
+								<div class="px-4">
+									<hr style="border-color: rgba(255,255,255,0.1);">
+								</div>
+							</li>
+							<li>
+								<a role="button" class="px-4 py-3" title="Meus personagens">
+									<i class="sidebar__menu__i far fa-plus-square fa-lg"></i>
+									<span>Criar Bolão</span>
+								</a>
+							</li>
+							<li>
+								<a role="button" class="px-4 py-3" title="Meus personagens">
+									<i class="sidebar__menu__i far fa-futbol fa-lg"></i>
+									<span>Meus Bolões</span>
+								</a>
+							</li>
+							<li>
+								<a role="button" class="px-4 py-3" title="Meus personagens">
+									<i class="sidebar__menu__i fas fa-futbol fa-lg"></i>
+									<span>Bolões que participo</span>
+								</a>
+							</li>
+							<li>
+								<a role="button" class="px-4 py-3" title="Logs">
+									<i class="sidebar__menu__i fas fa-file-alt fa-lg"></i>
+									<span>Histórico</span>
+								</a>
+							</li>
+							<li>
+								<a role="button" class="px-4 py-3" title="Sair">
+									<i class="sidebar__menu__i fas fa-sign-out-alt fa-lg"></i>
+									<span>Sair</span>
+								</a>
+							</li>
+						</ul>
+					</nav><!-- /.sidebar__menu !-->
+				</aside><!-- /#sidebar-mobile !-->
+				<section class="dashboard-body__content px-4 py-4">
+					<nav aria-label="breadcrumb" class="mb-4">
+						<div class="container">
+							<div class="row">
+								<ol class="breadcrumb bg-transparent m-0 small">
+									<li class="breadcrumb-item">
+										<a href="#">Inicio</a>
+									</li>
+									<li class="breadcrumb-item active" aria-current="page">Meus Bolões</li>
+								</ol>
+							</div><!-- /.row !-->
+						</div><!-- /.container !-->
+					</nav><!-- /breadcrumb !-->
+					<div class="row">
+						<div class="col-12">
+							<div class="card border-0 rounded p-3 blur-shadow mr-0">
+								<div class="card-header bg-white">
+									<div class="row">
+										<div class="col-6 pt-2">
+											<h5 class="text-muted font-weight-light">Meus Bolões</h5>
+										</div>
+									</div>
+								</div><!-- /.card-header !-->
+								<div class="card-body">
+									<div class="table-responsive-sm">
+										<table class="table table-hover small">
+											<thead class="thead-light">
+												<tr>
+													<th scope="col">Tipo</th>
+													<th scope="col">Nome</th>
+													<th scope="col" class="d-none d-md-table-cell">Campeonato</th>
+													<th scope="col" class="d-none d-md-table-cell">Rodada</th>
+													<th scope="col">Encerramento</th>
+													<th scope="col">Ações</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>
+														<i class="fas fa-lock-open text-black-50" title="Público - Aberto à todos"></i>
+													</td>
+													<td>Bolão do Ivan</td>
+													<td class="d-none d-md-table-cell">Campeonato Brasileiro - Série A - 2019</td>
+													<td class="d-none d-md-table-cell">2ª</td>
+													<td>15/12/2018</td>
+													<td>
+                                                        <a href="#!" class="btn btn-sm btn-pal-1">
+															<small>Cadastrar Resultados</small>
+														</a>
+														<a href="#!" class="btn btn-sm btn-pal-2">
+															<small>Detalhes</small>
+														</a>
+														<a href="#!" class="btn btn-sm btn-danger">
+															<small>Excluir</small>
+														</a>
+													</td>
+                                                </tr>
+                                                <tr>
+													<td>
+														<i class="fas fa-lock text-black-50" title="Público - Aberto à todos"></i>
+													</td>
+													<td>Bolão da turma</td>
+													<td class="d-none d-md-table-cell">Campeonato Brasileiro - Série A - 2019</td>
+													<td class="d-none d-md-table-cell">2ª</td>
+													<td>18/12/2018</td>
+													<td>
+                                                        <a href="#!" class="btn btn-sm btn-pal-1">
+															<small>Cadastrar Resultados</small>
+														</a>
+														<a href="#!" class="btn btn-sm btn-pal-2">
+															<small>Detalhes</small>
+														</a>
+														<a href="#!" class="btn btn-sm btn-danger">
+															<small>Excluir</small>
+														</a>
+													</td>
+												</tr>
+											</tbody>
+										</table>
+									</div><!-- /.table-responsive-sm !-->
+								</div><!-- /.card-body !-->
+							</div><!-- /.card !-->
+						</div>
+                    </div><!-- /.row !-->
+                    <div class="row mt-4">
+                        <div class="col-12 col-md-11">
+
+                            <nav aria-label="navigation">
+                                <ul class="pagination justify-content-center small">
+                                    <li class="page-item disabled">
+                                        <a class="page-link" href="#" tabindex="-1">Anterior</a>
+                                    </li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">1</a>
+                                    </li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">2</a>
+                                    </li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">3</a>
+                                    </li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">Próximo</a>
+                                    </li>
+                                </ul>
+                            </nav>
+
+                        </div>
+                    </div><!-- /.row !-->
+				</section>
+			</main>
+
+			<script src="../../dist/public/js/utils.min.js"></script>
+			<script src="../../dist/admin/js/scripts.min.js"></script>
+		</div><!-- /.page-wrap !-->
+	</body>
+</html>
