a151e2fda72083f1aa1435e673b802dc0275da1b
Lucas Santos <lucasljsb@gmail.com>
2018-12-14 20:12:26 -0300

    Adicionados links à o template header

--- dist/application/views/header.php
+++ dist/application/views/header.php
@@ -27,7 +27,7 @@
 				<div class="top-header__container container px-md-2 py-2 rounded">
 					<div class="row m-0">
 						<nav class="navbar col-12 col-md-4 text-center text-md-left">
-							<a class="navbar-brand mx-auto ml-md-0 mr-md-3" href="#">
+							<a class="navbar-brand mx-auto ml-md-0 mr-md-3" href="<?php echo $links['home']; ?>">
 								<img src="<?php echo $base_url; ?>/public/img/logo-slogan.svg" class="d-inline-block align-middle" width="200" alt="Logotipo sisbolão">
 							</a>
 						</nav>
@@ -35,19 +35,19 @@
 							<div class="collapse navbar-collapse">
 								<ul class="navbar-nav mr-auto">
 									<li class="nav-item active px-md-2">
-										<a role="button" href="#!" class="nav-link">Inicio <span class="sr-only">(atual)</span></a>
+										<a role="button" href="<?php echo $links['home']; ?>" class="nav-link">Inicio <span class="sr-only">(atual)</span></a>
 									</li>
 									<li class="nav-item px-md-2">
-										<a role="button" href="#!" class="nav-link">Bolões</a>
+										<a role="button" href="<?php echo $links['betting-sets']; ?>" class="nav-link">Bolões</a>
 									</li>
 									<li class="nav-item px-md-2">
-										<a role="button" href="#!" class="nav-link">Ranking</a>
+										<a role="button" href="<?php echo $links['ranking']; ?>" class="nav-link">Ranking</a>
 									</li>
-									<li class="nav-item px-md-2 d-none">
-										<a role="button" href="#!" class="btn btn-md btn-pal-2 text-white">Cadastre-se</a>
+									<li class="nav-item px-md-2 <?php echo ($current_page === 'home' ? 'd-none' : ''); ?>">
+										<a role="button" href="<?php echo $links['register']; ?>" class="btn btn-md btn-pal-2 text-white">Cadastre-se</a>
 									</li>
 									<li class="nav-item px-md-2">
-									<a role="button" href="#!" class="btn btn-md btn-pal-1 text-white">Fazer Login</a>
+									<a role="button" href="<?php echo $links['login']; ?>" class="btn btn-md btn-pal-1 text-white">Fazer Login</a>
 									</li>
 								</ul>
 							</div>
