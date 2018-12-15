293c077171f811a014f6fc0ee12216c435aee227
Lucas Santos <lucasljsb@gmail.com>
2018-12-14 22:12:59 -0300

    Implementada funcionalidade cadastro

--- dist/application/controllers/Users.php
+++ dist/application/controllers/Users.php
@@ -10,29 +10,13 @@ class Users extends REST_Controller {
     {
         parent::__construct();
         $this->load->model('users_model');
+        $this->load->database(); # Usado somente em contexto de validação
     }
 
     /**
      * 
      */
     public function index_get()
-    {
-        $result = $this->users_model->get();
-        if ( (is_array($result) && ! empty(array_filter($result)))
-                || $result !== NULL )
-        {
-            $this->set_response($result, REST_Controller::HTTP_OK);
-        }
-        else
-        {
-            $this->set_response([
-                'status' => FALSE,
-                'message' => 'No registered users'
-            ], REST_Controller::HTTP_NO_CONTENT);
-        }
-    }
-
-    public function users_get()
     {
         $id = $this->get('id');
         if ($id === NULL || ! is_int($id) || $id < 0)
@@ -58,7 +42,74 @@ class Users extends REST_Controller {
                 ], REST_Controller::HTTP_NOT_FOUND);
             }
         }
-        
+    }
+
+    public function index_post()
+    {
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+
+        $result = array(); # saída
+
+        # Mensagem padrão de campo obrigatório
+        $required_message = 'O campo {field} é obrigatório';
+
+        # Regras de validação do campo nome
+        $this->form_validation->set_rules(
+            'name', 'Nome', 
+            'required', 
+            array(
+                'required' => $required_message
+            )
+        );
+
+        # Regras de validação do campo e-mail
+        $this->form_validation->set_rules(
+            'email', 'E-mail', 
+            'required|valid_email|is_unique[users.email]', 
+            array(
+                'required' => $required_message,
+                'valid_email' => 'Formato de e-mail inválido',
+                'is_unique' => 'Este e-mail já está sendo utilizado'
+            )
+        );
+
+        # Regras de validação do campo senha
+        $this->form_validation->set_rules(
+            'password', 'Senha', 
+            'required',
+            array(
+                'required' => $required_message
+            )
+        );
+
+        # Regras de validação do campo de confirmação de senha
+        $this->form_validation->set_rules(
+            'confirm_password', 'Confirmação da senha', 
+            'required|matches[password]',
+            array(
+                'required' => $required_message,
+                'matches' => 'As senhas informadas não conferem'
+            )
+        );
+
+        # Teste de validação
+        if ($this->form_validation->run() !== FALSE)
+        {
+            $this->users_model->new($this->input->post(NULL, TRUE));
+            $this->set_response([
+                'status' => TRUE,
+                'message' => 'Cadastro realizado com sucesso!'
+            ], REST_Controller::HTTP_CREATED);
+        }
+        else
+        {
+            $this->set_response([
+                'status' => FALSE,
+                'message' => 'Falha ao realizar o cadastro.',
+                'errors' => $this->form_validation->error_array()
+            ], REST_Controller::HTTP_CREATED);
+        }
     }
 
 }
--- dist/application/models/Users_model.php
+++ dist/application/models/Users_model.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Users_model extends SISB_model {
+class Users_model extends SISB_Model {
 
     public function __construct()
     {
@@ -29,6 +29,27 @@ class Users_model extends SISB_model {
         $query = $qb->getQuery();
 
         return $this->result($query, $result_mode);        
+    }
+
+    public function new($form_data = array())
+    {
+        if ( ! empty(array_filter($form_data)))
+        {
+            $user = new \Entity\User();
+            $user->set_access_level(0); # usuário comum (não administrador)
+            $user->set_state(0); # status da conta; 0 = normal
+            $user->set_name($form_data['name']);
+            $user->set_email($form_data['email']);
+            $user->set_password($form_data['password']); # criptografar a senha?
+
+            # Iniciar persistência
+            $this->doctrine->get_entity_manager()->persist($user);
+            $this->doctrine->get_entity_manager()->flush();
+        }
+        else
+        {
+            // exception ?
+        }
     }
 
 }
